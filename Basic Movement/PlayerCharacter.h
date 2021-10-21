4e1ecdecc6a218a77ef4f183729cce6db7a2e438
AlternateWalls <71268229+AlternateWalls@users.noreply.github.com>
2021-10-21 07:09:54 -0400

    Update PlayerCharacter.h

--- Basic Movement/PlayerCharacter.h
+++ Basic Movement/PlayerCharacter.h
@@ -1,3 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PlayerCharacter.generated.h"
+
+UENUM()
+enum PlayerOrientation
+{
+	Normal,
+	Flipped
+};
+
 UCLASS()
 class VERTIGO_API APlayerCharacter : public ACharacter
 {
@@ -28,3 +43,53 @@ protected:
 	//UFUNCTION()
 	//void FlipDown(FHitResult hit);
 	///FLIPPING FUNCTIONS (EXPOSED)
+
+	///INSTANCES (EXPOSED)
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		class UCameraComponent* Camera;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UCurveFloat* z_bottomCurve;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UCharacterMovementComponent* Movement;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TEnumAsByte<PlayerOrientation> orientation;
+	///INSTANCES (EXPOSED)
+
+	///SPEED VARIABLES (EXPOSED)
+	float baseSpeed = 2000;
+	///SPEED VARIABLES (EXPOSED)
+
+	///FLIPPING VARIABLES (EXPOSED)
+	float minFlipSpeed = 2000; //Around 2000
+	float flipCurve = 2400.0f;
+	///FLIPPING VARIABLES (EXPOSED)
+
+
+private:
+	///FLIPPING FUNCTIONS (HIDDEN)
+	void HandleOrientation();
+
+	void SetupGroundConditions();
+	void SetFlipConditions();
+	void SetupCeilingConditions();
+
+	FVector FlipLineStart();
+	FVector FlipLineEnd();
+	///FLIPPING FUNCTIONS (HIDDEN)
+
+	///GETTER FUNCTIONS (HIDDEN)
+	FVector PlayerLocation();
+	FRotator PlayerRotation();
+	///GETTER FUNCTIONS (HIDDEN)
+
+	///TIMERS (HIDDEN)
+	FTimerHandle FlipTimer;
+	///TIMERS (HIDDEN)
+
+	///FLIPPING VARIABLES (HIDDEN)
+	float currentCameraRoll;
+	float storedVelocityMagnitude;
+	FVector originalDistance;
+	///FLIPPING VARIABLES (HIDDEN)
+
+};
